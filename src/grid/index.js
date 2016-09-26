1e70ca494eda711bc097548d1eb3e4b021bf43c4
extronics <post@calitrix.de>
2016-09-26 17:35:51 +0200

    Add index.js for grid module

---
+++ src/grid/index.js
@@ -0,0 +1,4 @@
+export {default as GaussianGrid} from './GaussianGrid'
+export {default as Grid} from './Grid'
+export {default as GridFactory} from './GridFactory'
+export {default as GridSet} from './GridSet'
