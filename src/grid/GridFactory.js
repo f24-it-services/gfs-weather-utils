3331536543fe28a0ee930f0071f7cd25e788a002
spurreiter <spurreiter@users.noreply.github.com>
2019-04-26 08:57:27 +0200

    chore: upgrade to babel@7

--- src/grid/GridFactory.js
+++ src/grid/GridFactory.js
@@ -7,17 +7,17 @@ export default {
       throw new Error('fromJSON() needs an array with exactly one element')
     }
 
-    let {header, data} = json[0]
+    let { header, data } = json[0]
 
     switch (header.gridDefinitionTemplate) {
       // Default Lat/Lng grid
       case 0: {
-        let {la1, lo1, dx, dy, nx, ny} = header
+        let { la1, lo1, dx, dy, nx, ny } = header
         return new Grid(0, la1, lo1, dx, dy, nx, ny, data)
       }
       // Gaussian Lat/Lng grid
       case 40: {
-        let {lo1, la1, lo2, la2, np, nx, ny, dx, dy} = header
+        let { lo1, la1, lo2, la2, np, nx, ny, dx, dy } = header
         return new GaussianGrid(40, lo1, la1, lo2, la2, np, nx, ny, dx, dy, data)
       }
     }
